320b6ab49902a7cb2c47548e6b835176e1b6a2e2
szabolcsit <szabolcsit@hu.ibm.com>
2019-02-18 19:11:10 +0100

    more content

--- src/components/Pic/index.js
+++ src/components/Pic/index.js
@@ -2,14 +2,13 @@ import React from 'react'
 import styles from './pic.module.scss'
 
 const Pic = ({ alt, src, title }) => {
-  const pull = alt === 'left' ? 'left' : 'right'
   const classNames = [styles.image]
 
-  classNames.push(styles[`image__pull_${pull}`])
+  classNames.push(styles[`image__pull_${alt}`])
 
   return (
     <span className={classNames.join(' ')}>
-      <img src={src} alt={title} />
+      <img src={src} alt={title} className={styles[`image__pull_${alt}`]} />
       <small className={styles.image_description}>{title}</small>
     </span>
   )
